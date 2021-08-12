70b679aa671798bee222e62b2156ca22bef70340
Vegeta Saiyan <sadmansamin225@gmail.com>
2021-08-12 17:55:54 -0400

    completed task 1

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category){
+  
+  return {name: name, price: price, category: category};
+  
+
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -29,6 +32,9 @@ Test your createMenuItems function by doing the following:
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
 
+console.log(createMenuItem('Tacos', 8, 'Lunch'));
+console.log(createMenuItem('Pizza', 5 , 'Lunch'));
+console.log(createMenuItem('Global warming', 0 , 'Future'));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
